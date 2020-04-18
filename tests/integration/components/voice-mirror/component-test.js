09d257ef166193d21dab924eda332a156bffa14f
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2020-04-18 15:57:51 -0300

    Add link to mirror route on index and Add component tests to voice-mirror

--- tests/integration/components/voice-mirror/component-test.js
+++ tests/integration/components/voice-mirror/component-test.js
@@ -1,12 +1,29 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | voice-mirror', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    assert.ok(true);
+    await render(hbs`<VoiceMirror />`);
+    assert.equal(
+      this.element.textContent.trim(),
+      'Voice Mirror',
+      'It has a title'
+    );
+
+    assert.equal(
+      this.element.querySelector('[data-test-start-voice-mirror').value.trim(),
+      'Start',
+      'it has a start button'
+    );
+
+    assert.equal(
+      this.element.querySelector('[data-test-pause-voice-mirror').value.trim(),
+      'Pause',
+      'it has a pause button'
+    );
   });
 });
