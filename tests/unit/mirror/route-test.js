917fa7fb3316b768a8775bd00ef8e4d7fdba77b5
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2020-04-19 00:02:08 -0300

    Fix Sonar code smells

--- tests/unit/mirror/route-test.js
+++ tests/unit/mirror/route-test.js
@@ -5,7 +5,7 @@ module('Unit | Route | mirror', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    let route = this.owner.lookup('route:mirror');
+    const route = this.owner.lookup('route:mirror');
     assert.ok(route);
   });
 });
