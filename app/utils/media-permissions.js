ded1c8c6502a9f8d305b0330c7c1566e8aab14eb
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2020-04-18 23:45:53 -0300

    Add sinon dependecy and add tests for media-poermissions utils

--- app/utils/media-permissions.js
+++ app/utils/media-permissions.js
@@ -1,7 +1,6 @@
 export default async function getMediaDevicesPermissions() {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: false,
   });
   const audio = new Audio();
   audio.srcObject = stream;
