6d0f16ec5c02b4b9a5ab7a264243459989ba9fd4
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2021-05-21 22:05:43 -0300

    Add flags and ceate an app-link component

--- app/index/route.js
+++ app/index/route.js
@@ -1,3 +1,14 @@
 import Route from '@ember/routing/route';
 
-export default class IndexRoute extends Route {}
+export default class IndexRoute extends Route {
+  model() {
+    return [
+      {
+        route: 'mirror',
+        icon: '🎙',
+        title: 'Voice Mirror',
+        description: 'Check if your mic is working correctly',
+      },
+    ];
+  }
+}
